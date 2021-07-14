0b8b60a5b238ef29588316c813c5522e4cf63a91
zevdvlpr <zevdvlpr@gmail.com>
2021-07-14 20:52:59 -0300

    ✨ feat: colors

---
+++ src/config/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  primary: '#F04747',
+  gray: {
+    '300': '#C4C4C4',
+    '500': '#737373',
+  },
+  black: '#000000',
+  white: '#FFFFFF',
+};
+
+export function getColors() {
+  return colors;
+}
+
+export type TColors = ReturnType<typeof getColors>;
