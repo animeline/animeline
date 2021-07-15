9d632622de0c755ce79c2a3dae10363749934d05
zevdvlpr <zevdvlpr@gmail.com>
2021-07-15 18:01:40 -0300

    🔨 refactor: rename all colors

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,9 +1,9 @@
 export const colors = {
   primary: '#F04747',
-  gray: {
-    '300': '#C4C4C4',
-    '500': '#737373',
-  },
+
+  'gray.300': '#C4C4C4',
+  'gray.500': '#737373',
+
   black: '#000000',
   white: '#FFFFFF',
 };
