612b486b427858baf085a8fd8eca47de5208be25
zevdvlpr <zevdvlpr@gmail.com>
2021-07-14 21:00:52 -0300

    ✨ feat: stack routes

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const {Navigator, Screen} = createStackNavigator();
+
+import {Anime} from '../screens/Anime';
+import {Search} from '../screens/Search';
+import {Onboarding} from '../screens/Onboarding';
+
+import {TabRoutes} from './tab.routes';
+
+export function StackRoutes() {
+  return (
+    <Navigator screenOptions={{headerShown: false}}>
+      <Screen name="Home" component={TabRoutes} />
+      <Screen name="Onboarding" component={Onboarding} />
+      <Screen name="Anime" component={Anime} options={{headerShown: true}} />
+      <Screen name="Search" component={Search} options={{headerShown: true}} />
+    </Navigator>
+  );
+}
