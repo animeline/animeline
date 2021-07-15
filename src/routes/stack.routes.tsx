304121e024e34d9a38d1a419bfae243e78cd41da
zevdvlpr <zevdvlpr@gmail.com>
2021-07-15 14:02:47 -0300

    ✨ feat: add settings screen to stack

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,6 +5,8 @@ const {Navigator, Screen} = createStackNavigator();
 
 import {Anime} from '../screens/Anime';
 import {Search} from '../screens/Search';
+import {Settings} from '../screens/Settings';
+
 import {Onboarding} from '../screens/Onboarding';
 
 import {TabRoutes} from './tab.routes';
@@ -16,6 +18,11 @@ export function StackRoutes() {
       <Screen name="Onboarding" component={Onboarding} />
       <Screen name="Anime" component={Anime} options={{headerShown: true}} />
       <Screen name="Search" component={Search} options={{headerShown: true}} />
+      <Screen
+        name="Settings"
+        component={Settings}
+        options={{headerShown: true}}
+      />
     </Navigator>
   );
 }
