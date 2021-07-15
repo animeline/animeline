718b0a0d7291065cdf311528aa5fbf174329148c
zevdvlpr <zevdvlpr@gmail.com>
2021-07-14 21:00:16 -0300

    ✨ feat: categories page

---
+++ src/screens/Categories/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {ServiceUnavailable} from '../../components/ServiceUnavailable';
+
+import {Default} from '../../layout/Default';
+
+import {Content} from './styles';
+
+export function Categories() {
+  return (
+    <Default>
+      <Content>
+        <ServiceUnavailable />
+      </Content>
+    </Default>
+  );
+}
