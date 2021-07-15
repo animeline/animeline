8a321aa393dd027eb2f5574a667dfb9c83d8124b
zevdvlpr <zevdvlpr@gmail.com>
2021-07-14 21:00:08 -0300

    ✨ feat: home page

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+import {useTheme} from 'styled-components';
+import {useNavigation} from '@react-navigation/native';
+
+import {Default} from '../../layout/Default';
+
+import {Content} from './styles';
+
+export function Home() {
+  const theme = useTheme();
+
+  const navigation = useNavigation();
+
+  return (
+    <Default
+      showHeader
+      statusBar={{
+        backgroundColor: theme.primary,
+        barStyle: 'default',
+      }}>
+      <Content>
+        <TouchableOpacity onPress={() => navigation.navigate('Anime')}>
+          <Text>Navegar para o Anime</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Onboarding')}>
+          <Text>Navegar para o Onboarding</Text>
+        </TouchableOpacity>
+      </Content>
+    </Default>
+  );
+}
