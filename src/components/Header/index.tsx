6928548569bf69701843cf9ef47e4e0365eacbc8
zevdvlpr <zevdvlpr@gmail.com>
2021-07-16 12:58:14 -0300

    ✨ feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {ColorValue, View} from 'react-native';
+import {useTheme} from 'styled-components';
 
-// import {LogoText} from '../../assets/images/svg/logo_text';
+import {Default} from './Types/Default';
+import {Back} from './Types/Back';
 
-import {Container} from './styles';
+interface HeaderProps {
+  type?: 'default' | 'back';
+  bgColor?: ColorValue;
+  hidden?: boolean;
+}
 
-export function Header() {
+export function Header({
+  type = 'default',
+  bgColor = 'white',
+  hidden = true,
+}: HeaderProps) {
+  const route = useRoute();
+
+  const navigation = useNavigation();
+
+  const theme = useTheme();
+
+  function handleNavigateToSettingsScreen() {
+    navigation.navigate('Settings');
+  }
+
+  function handleNavigateGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Container>
-      <Text>Animeline</Text>
-    </Container>
+    <View>
+      {!hidden && type === 'default' && (
+        <Default
+          theme={theme}
+          onNavigateToSettingsScreen={handleNavigateToSettingsScreen}
+        />
+      )}
+
+      {!hidden && type === 'back' && (
+        <Back
+          theme={theme}
+          bgColor={bgColor}
+          routeName={route.name}
+          onNavigateGoBack={handleNavigateGoBack}
+        />
+      )}
+    </View>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,12 +5,23 @@ export const Container = styled.View`
   width: 100%;
   height: 60px;
 
-  padding: 0 10px;
+  padding: 0 20px;
   padding-horizontal: ${getStatusBarHeight() + 'px'};
 
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
 
+  elevation: 5;
+
   background-color: ${props => props.theme.primary};
+`;
+
+export const Title = styled.Text`
+  margin-left: 25px;
+
+  font-size: 20px;
+  font-family: 'Jost Regular';
+
+  color: ${props => props.theme['gray.500']};
 `;
