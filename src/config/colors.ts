4e33c3f9a572ef2d457f76eac2fb799c021254c0
zevdvlpr <zevdvlpr@gmail.com>
2021-07-16 12:35:48 -0300

    ✨ feat: new colors

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,9 +1,10 @@
 export const colors = {
   primary: '#F04747',
-
   'gray.300': '#C4C4C4',
   'gray.500': '#737373',
-
+  'gold.300': '#fAA81A',
+  'red-light.300': '#ED4245',
+  'green-dark.500': '#3BA55D',
   black: '#000000',
   white: '#FFFFFF',
 };
